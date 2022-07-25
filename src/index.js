0e9f877e414bb94d18af338de835682152f5a749
Gabriel Freire Braz <gabrielfreirebraz@gmail.com>
2022-07-25 00:32:33 +0000

    feat: create middleare function and create user and get todo routes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,59 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const usernameRequest = request.headers.username;
+  const isUserExists = users.find(function(elem){
+
+    return elem.username === usernameRequest;
+  });
+
+
+  if ( !isUserExists ) {
+    return response.status(400).json( {errorMessage: "User not found, please try again!"} );
+  }
+  else {
+
+    request.currentUser = isUserExists;
+    return next();
+  }  
 }
 
+
+
+
+
 app.post('/users', (request, response) => {
-  // Complete aqui
+
+
+  let myReq = {
+    id: uuidv4(),
+    name: request.body.name,
+    username: request.body.username,
+    todos: []
+  }
+
+  users.push(myReq)
+
+  return response.status(200).json( users );
 });
 
+
+
+ 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+
+
+  return response.status(200).json( request.currentUser.todos );
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
